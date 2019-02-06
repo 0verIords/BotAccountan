da295a6fb3045f820160b1bedd7f69ad33b27401
Meder <meder33kg@gmail.com>
2019-02-06 19:29:19 +0600

    fix incorrect download

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"time"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/rylio/ytdl"
 )
@@ -14,23 +16,19 @@ func youtubeDown(url string) {
 	file, _ := os.Create(vid.ID + ".mp3")
 
 	defer file.Close()
-
-	vid.Download(vid.Formats[17], file)
-
+	vid.Download(vid.Formats[13], file)
 }
 
 func getVideoID(url string) (string, bool) {
 	if strings.Contains(url, "=") {
 		urls := strings.Split(url, "=")
 		if len(urls[len(urls)-1]) == 11 {
-			youtubeDown(url)
 			return urls[len(urls)-1], true
 		}
 	}
 	if strings.Contains(url, "/") {
 		urls := strings.Split(url, "/")
 		if len(urls[len(urls)-1]) == 11 {
-			youtubeDown(url)
 			return urls[len(urls)-1], true
 		}
 
@@ -54,34 +52,58 @@ func main() {
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
+
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
+		link := update.Message.Text
+		user := update.Message.Chat.ID
+		finish := make(chan bool)
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		go func() {
+			log.Printf("[%s] %s", update.Message.From.UserName, link)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
-		if strings.Contains(update.Message.Text, "youtu.be") || strings.Contains(update.Message.Text, "https://www.youtube.com/watch?v") {
-			msg.Text = "music on the way"
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-			id, allow := getVideoID(update.Message.Text)
+			if strings.Contains(link, "youtu.be") || strings.Contains(link, "https://www.youtube.com/watch?v") {
+				go func() {
+					for {
+						select {
+						case <-finish:
+							close(finish)
+							return
+						default:
+							msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+							time.Sleep(time.Second * 30)
+							msg.Text = "file is download"
+							bot.Send(msg)
+						}
+					}
+				}()
+				msg.Text = "music on the way"
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+				id, allow := getVideoID(link)
 
-			if allow {
-				newVideo := tgbotapi.NewAudioUpload(update.Message.Chat.ID, id+".mp3")
-				if _, err := bot.Send(newVideo); err != nil {
-					log.Panic(err)
+				if allow {
+					youtubeDown(link)
+					newVideo := tgbotapi.NewAudioUpload(user, id+".mp3")
+					if _, err := bot.Send(newVideo); err != nil {
+						log.Panic(err)
+					}
+					finish <- true
+				} else {
+					msg.Text = "incorrect link"
+					bot.Send(msg)
+					finish <- true
 				}
 			} else {
-				msg.Text = "incorrect link"
+				msg.Text = "paste only the link to music"
+				msg.ReplyToMessageID = update.Message.MessageID
 				bot.Send(msg)
+				finish <- true
 			}
-		} else {
-			msg.Text = "paste only the link to music"
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-		}
+		}()
 	}
 }
