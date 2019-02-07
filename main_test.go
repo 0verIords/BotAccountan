9df3f733ad64ea5e2aeeacfe7cfe190b35488509
Meder <meder33kg@gmail.com>
2019-02-07 18:58:56 +0600

    Test coverage 20%

--- main_test.go
+++ main_test.go
@@ -1,21 +1,43 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestGetRightLink(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetVideoId(t *testing.T) {
 	var link = []struct {
-		TestLink    string
-		CorrectLink string
+		TestLink string
+		VideoId  string
 	}{
-		{"Rag'n'Bone Man - Human (Official Video) https://www.youtube.com/watch?v=L3wKzyIN1yk", "https://www.youtube.com/watch?v=L3wKzyIN1yk"},
-		{"Bon Jovi - It's My Life https://youtu.be/vx2u5uUu3DE", "https://youtu.be/vx2u5uUu3DE"},
-		{"https://www.youtube.com/watch?v=0I647GU3Jsc", "https://www.youtube.com/watch?v=0I647GU3Jsc"},
+		{"Rag'n'Bone Man - Human (Official Video) https://www.youtube.com/watch?v=L3wKzyIN1yk", "L3wKzyIN1yk"},
+		{"https://www.youtube.com/watch?v=L3wKzyIN1y", "fail"},
+		{"Bon Jovi - It's My Life https://youtu.be/vx2u5uUu3DE", "vx2u5uUu3DE"},
+		{"https://www.youtube.com/watch?v=", "fail"},
+		{"https://www.youtube.com/watch?v=0I647GU3Jsc", "0I647GU3Jsc"},
+		{"https://www.youtube.com/watch?v0I647GU3Jsc", "fail"},
+		{"https://youtu.be/vx2u5uUuDE", "fail"},
 	}
 
 	for _, tt := range link {
-		checkLink := getRightLink(tt.TestLink)
-		if tt.CorrectLink != checkLink {
-			t.Errorf("Не правильная передача файла (%s) != (%s)", tt.CorrectLink, tt.TestLink)
+		checkLink, _ := getVideoID(tt.TestLink)
+		assert.Equal(t, checkLink, tt.VideoId, "they should be equal")
+
+	}
+}
+
+func TestYoutubeDown(t *testing.T) {
+	testCases := []string{
+		"https://www.youtube.com/watch?v=hT_nvWreIhg",
+	}
+	for _, url := range testCases {
+		videoId, _ := getVideoID(url)
+		youtubeDown(url)
+		if _, err := os.Stat(videoId + ".mp3"); os.IsNotExist(err) {
+			t.Error("Файла не существует")
 		}
+
 	}
 }
